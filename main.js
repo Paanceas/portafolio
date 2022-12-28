d8371e5516f961bca8dfd969ec3071071fe06860
Pablo Ceballos <pablo-s_972011@hotmail.com>
2022-12-28 15:55:00 -0500

    implement user agent

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-
 function openClick() {
-    window.location.replace("https://segurosdaviplata.cardif.com.co/web/app-seguros-personas/tus-seguros-2");
+  let url =
+    "https://segurosdaviplata.cardif.com.co/web/app-seguros-personas/tus-seguros-2";
+  if (
+    navigator.userAgent.match(/iPhone/i) ||
+    navigator.userAgent.match(/iPad/i) ||
+    navigator.userAgent.match(/iPod/i)
+  ) {
+    url = "https://apps.apple.com/co/app/safari/id1146562112"
+  } else if (navigator.userAgent.match(/Android/i)) {
+    url = "https://play.google.com/store/apps/details?id=com.android.chrome&hl=es_CO&gl=US"
+  }
+  window.location.replace(url);
 }
